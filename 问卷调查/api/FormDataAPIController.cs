f80afbeffb60fe3167d9901d69d858708158bf40
xinwyaphtes <272566853@qq.com>
2019-04-19 18:06:15 +0800

    更新

---
+++ 问卷调查/api/FormDataAPIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using 问卷调查.Models;
+using 问卷调查.Util;
+
+namespace 问卷调查.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormDataAPIController : ControllerBase
+    {
+        [HttpGet]
+        public List<FormData> Get(string guid)
+        {
+            using (var db = DBHelp.QueryDB())
+            {
+                return db.Queryable<FormData>().Where(x => x.MainGuid == guid).ToList();
+            }
+        }
+    }
+}
