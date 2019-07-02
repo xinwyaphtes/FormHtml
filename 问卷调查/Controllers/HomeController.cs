31594a3dd3e1b476b58b1f0baad4b6e27c1592f9
xinwyaphtes <272566853@qq.com>
2019-07-02 15:41:27 +0800

    更新

--- 问卷调查/Controllers/HomeController.cs
+++ 问卷调查/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SqlSugar;
 using 问卷调查.Models;
 using 问卷调查.Util;
 
@@ -64,6 +65,44 @@ namespace 问卷调查.Controllers
                 ViewBag.RecordsCount = m.Count;
 
                 return PartialView("MainList", m);
+            }
+        }
+
+        public IActionResult HistoryForm(string cardNo, string name)
+        {
+            using (var db = DBHelp.QueryDB())
+            {
+                var list = db.Queryable<Main, Patient>((st, sc) => new object[] {
+        JoinType.Inner,st.VisitID==sc.VisitID})
+      .Select((st, sc) => new { st.Id, st.UpdateDT, sc.Name, sc.EmployeeNo }).MergeTable().WhereIF(!string.IsNullOrEmpty(cardNo), it => it.EmployeeNo == cardNo)
+        .WhereIF(!string.IsNullOrEmpty(name), it => it.Name == name).OrderBy(st => st.UpdateDT, OrderByType.Desc).Take(10).ToList();
+
+                List<HistoryData> hflist = new List<HistoryData>();
+
+                foreach (var item in list)
+                {
+                    HistoryData hf = new HistoryData
+                    {
+                        Id = item.Id,
+                        EmployeeNo = item.EmployeeNo,
+                        Name = item.Name,
+                        UpdateDT = (DateTime)item.UpdateDT
+                    };
+
+                    hflist.Add(hf);
+                }
+
+                return View(hflist);
+            }
+        }
+
+        public JsonResult ShowHisDataById(int id)
+        {
+            using (var db = DBHelp.QueryDB())
+            {
+                var history = db.SqlQueryable<FormData>("select FormData.* from Main,FormData where Main.Guid = FormData.MainGuid and Main.Id = " + id.ToString()).ToList();
+
+                return new JsonResult(history);
             }
         }
 
---
+++ 问卷调查/Models/HistoryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 问卷调查.Models
+{
+    public class HistoryData
+    {
+        public int Id { get; set; }
+        public DateTime UpdateDT { get; set; }
+        public string Name { get; set; }
+        public string EmployeeNo { get; set; }
+    }
+}
